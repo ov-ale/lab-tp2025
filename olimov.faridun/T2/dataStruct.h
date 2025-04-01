1e945e48afbdb06dc3dc6d22adcfae7e1ca6739c
Олимов Фаридун <maska192yt@mail.ru>
2025-04-02 00:38:44 +0300

    commit T2

---
+++ olimov.faridun/T2/dataStruct.h
@@ -0,0 +1,17 @@
+#ifndef DATASTRUCTH
+#define DATASTRUCTH
+
+#include <complex>
+#include <iosfwd>
+#include <string>
+
+struct DataStruct {
+    unsigned long long key1;
+    std::complex<double> key2;
+    std::string key3;
+};
+
+std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+std::istream& operator>>(std::istream& in, DataStruct& data);
+
+#endif#pragma once
---
+++ olimov.faridun/T2/main1.cpp
@@ -0,0 +1,46 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+
+#include "dataStruct.h"
+
+int main() {
+    std::vector<DataStruct> data;
+
+    while (!std::cin.eof()) {
+        std::copy(
+            std::istream_iterator<DataStruct>{std::cin},
+            std::istream_iterator<DataStruct>{},
+            std::back_inserter(data)
+        );
+
+        if (!std::cin) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1) {
+            return a.key1 < b.key1;
+        }
+
+        const double aMagnitude = std::abs(a.key2);
+        const double bMagnitude = std::abs(b.key2);
+        if (aMagnitude != bMagnitude) {
+            return aMagnitude < bMagnitude;
+        }
+
+        return a.key3.length() < b.key3.length();
+        });
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>{std::cout, "\n"}
+    );
+
+    return 0;
+}
