e8956c71f39b1c9a3dffbf8732a58bc27f6c58d3
Гусев Илья <ilya007gs@yandex.ru>
2025-04-02 16:28:28 +0300

    fix parsingFunctions.cpp

--- gusev.ilya/T2/dataStruct.h
+++ gusev.ilya/T2/dataStruct.h
@@ -10,11 +10,10 @@ struct DataStruct {
     std::string key3;
 };
 
-// Объявления функций
 bool parseScientificDouble(const std::string& str, double& value);
 bool parseLongLong(const std::string& str, long long& value);
 std::istream& operator>>(std::istream& in, DataStruct& data);
 std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 bool compareDataStruct(const DataStruct& a, const DataStruct& b);
 
-#endif // DATASTRUCT_H
+#endif
--- gusev.ilya/T2/parsingFunctions.cpp
+++ gusev.ilya/T2/parsingFunctions.cpp
@@ -11,14 +11,17 @@ bool parseScientificDouble(const std::string& str, double& value) {
 bool parseLongLong(const std::string& str, long long& value) {
     std::istringstream iss(str);
 
-    if (str.length() > 2 && str.substr(0, 2) == "0\\x" || str.substr(0, 2) == "0\\X") {
+    if (str.length() > 2 && (str.substr(0, 2) == "0\\x" || str.substr(0, 2) == "0\\X")) {
         iss >> std::hex >> value;
-    } else if (str.length() > 2 && str.substr(0, 2) == "0\\b" || str.substr(0, 2) == "0\\B") {
+    }
+    else if (str.length() > 2 && (str.substr(0, 2) == "0\\b" || str.substr(0, 2) == "0\\B")) {
         value = std::stoll(str.substr(2), nullptr, 2);
         return true;
-    } else if (str.length() > 1 && str.front() == '0') {
+    }
+    else if (str.length() > 1 && str.front() == '0') {
         iss >> std::oct >> value;
-    } else {
+    }
+    else {
         iss >> std::dec >> value;
     }
 
