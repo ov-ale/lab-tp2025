ca17546b7fcf45b705810d657d72da17defeb9a4
a93qwr124sqrqwrwq <iruthleesredx@gmail.com>
2025-04-02 21:49:31 +0300

    davletov.albert/T2

---
+++ davletov.albert/T2/DataStruct.h
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+namespace nspace {
+    struct Data {
+        unsigned long long key1;
+        unsigned long long key2;
+        std::string key3;
+    };
+
+    struct DelimiterIO {
+        char exp;
+    };
+
+    struct ULLLitIO {
+        unsigned long long& ref;
+    };
+
+    struct ULLHexIO {
+        unsigned long long& ref;
+    };
+
+    struct StringIO {
+        std::string& ref;
+    };
+
+    struct LabelIO {
+        std::string exp;
+    };
+
+    bool comparator(Data& first, Data& second);
+
+    class iofmtguard
+    {
+    public:
+        iofmtguard(std::basic_ios< char >& s);
+        ~iofmtguard();
+    private:
+        std::basic_ios< char >& s_;
+        std::streamsize width_;
+        char fill_;
+        std::streamsize precision_;
+        std::basic_ios< char >::fmtflags fmt_;
+    };
+
+
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+    std::istream& operator>>(std::istream& in, ULLLitIO&& dest);
+    std::istream& operator>>(std::istream& in, ULLHexIO&& dest);
+    std::istream& operator>>(std::istream& in, StringIO&& dest);
+    std::istream& operator>>(std::istream& in, LabelIO&& dest);
+    std::istream& operator>>(std::istream& in, Data& dest);
+    std::ostream& operator<<(std::ostream& out, const Data& dest);
+
+};
+
