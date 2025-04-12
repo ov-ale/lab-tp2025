87dd5a674c7f3e440ceba3b1c733245900c1d25e
EvgeniiMilo <it.evgenii2021@gmail.com>
2025-04-12 18:09:31 +0300

    commit new

--- miloradov.evgenii/T2/StreamGuard.cpp
+++ miloradov.evgenii/T2/StreamGuard.cpp
@@ -1,8 +1,6 @@
 #include "StreamGuard.h"
 
-StreamGuard::StreamGuard(std::ios& stream) : 
-	stream{stream}, flags{stream.flags()}, precision{stream.precision()} {
-}
+StreamGuard::StreamGuard(std::ios& stream) : stream{stream}, flags{stream.flags()}, precision{stream.precision()} {}
 
 StreamGuard::~StreamGuard() {
 	stream.precision(precision);
