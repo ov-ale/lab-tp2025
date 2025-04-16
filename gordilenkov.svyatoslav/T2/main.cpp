9a5c4060bab87895d6c22c50dfa8f19e39aeffcc
BaDRiVeR <sgordilenkov@mail.ru>
2025-04-16 13:51:44 +0300

    Fix (T2): replacing TABs to Spaces

--- gordilenkov.svyatoslav/T2/main.cpp
+++ gordilenkov.svyatoslav/T2/main.cpp
@@ -7,13 +7,13 @@ int main() {
     std::vector<DataStruct> dataVec;
 
     std::copy(std::istream_iterator<DataStruct>(std::cin),
-              std::istream_iterator<DataStruct>(),
-              std::back_inserter(dataVec));
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(dataVec));
 
     std::sort(dataVec.begin(), dataVec.end(), compareData);
-
+    
     std::copy(dataVec.begin(), dataVec.end(),
-              std::ostream_iterator<DataStruct>(std::cout, "\n"));
+            std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
